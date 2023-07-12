1827d33409dc9269ab8f8e7fb180e8b65648e343
Dimanqe <d.sankeev@gmail.com>
2023-07-12 15:25:30 +0300

    Добавьте файлы проекта.

---
+++ Итоговый проект 5.6.1 (HW-03)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Итоговый_проект_5._6._1__HW_03_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

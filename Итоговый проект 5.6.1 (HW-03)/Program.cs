ca40975fb540d2c3b1075128dd0b334f43d2ecb7
Dimanqe <d.sankeev@gmail.com>
2023-07-13 18:36:05 +0300

    Готовая программа

--- Итоговый проект 5.6.1 (HW-03)/Program.cs
+++ Итоговый проект 5.6.1 (HW-03)/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,105 @@ namespace Итоговый_проект_5._6._1__HW_03_
     {
         static void Main(string[] args)
         {
+            ShowData(EnterData());
         }
+        static (string Name, string LastName, int Аge, string[] PetNames, string[] Colors) EnterData()
+        {
+            (string Name, string LastName, int Аge, string[] PetNames, string[] Colors) User;
+            Console.WriteLine("Введите имя: ");
+            User.Name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию: ");
+            User.LastName = Console.ReadLine();
+            string age;
+            int intage;
+            do
+            {
+                Console.WriteLine("Введите возраст цифрами: ");
+                age = Console.ReadLine();
+            }
+            while (CheckNum(age, out intage));
+            User.Аge = intage;
+            Console.WriteLine("У вас есть домашние животные? Введите \"да\" или \"нет\"  ");
+            string hasPet = Console.ReadLine();
+            if (hasPet == "да")
+            {
+                int intPetCount;
+                string petCount;
+                do
+                {
+                    Console.WriteLine("Напишите их количество");
+                    petCount = Console.ReadLine();
+                }
+                while (CheckNum(petCount, out intPetCount));
+                User.PetNames = PetName(intPetCount);
+            }
+            else
+            {
+                User.PetNames = null;
+            }
+            string colorCount;
+            int intcolorCount;
+            do
+            {
+                Console.WriteLine("Введите количество любимых цветов");
+                colorCount = Console.ReadLine();
+            }
+            while (CheckNum(colorCount, out intcolorCount));
+            User.Colors = Colors(intcolorCount);
+            return User;
+        }
+        static string[] PetName(int petCount)
+        {
+            string[] Pets = new string[petCount];
+            for (int i = 0; i < Pets.Length; i++)
+            {
+                Console.WriteLine($"Введите кличку {i + 1}-го питомца");
+                Pets[i] = Console.ReadLine();
+            }
+            return Pets;
+        }
+        static string[] Colors(int colorCount)
+        {
+            string[] Colors = new string[colorCount];
+            for (int i = 0; i < Colors.Length; i++)
+            {
+                Console.WriteLine($"Введите название {i + 1}-го цвета");
+                Colors[i] = Console.ReadLine();
+            }
+            return Colors;
+        }
+        static bool CheckNum(string number, out int corrnumber)
+        {
+            if (int.TryParse(number, out int intnum))
+            {
+                if (intnum > 0)
+                {
+                    corrnumber = intnum;
+                    return false;
+                }
+            }
+            {
+                corrnumber = 0;
+                return true;
+            }
+
+        }
+        static void ShowData((string Name, string LastName, int Аge, string[] PetNames, string[] Colors) User)
+        {
+            Console.WriteLine("\n--------------------------------\n");
+            Console.WriteLine($"Ваши данные:\nИмя: {User.Name}\nФамилия: {User.LastName}\nВозраст: {User.Аge}\n");
+            Console.WriteLine("Питомцы:");
+            for (int i = 0; i < User.PetNames.Length; i++)
+            {
+                Console.WriteLine(User.PetNames[i]);
+            }
+            Console.WriteLine("\nЛюбимые цвета:");
+            for (int i = 0; i < User.Colors.Length; i++)
+            {
+                Console.WriteLine(User.Colors[i]);
+            }
+        }       
+
     }
+
 }
